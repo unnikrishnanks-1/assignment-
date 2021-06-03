2b5ce69fd49df1f7360aab5140887de312d09ba3
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-06-03 23:04:50 +0530

    v commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void test2(){
+        System.out.println("Hello World");
+    }
+
+
+    public static void main(String []args){
+        test2();
+    }
+}
+
